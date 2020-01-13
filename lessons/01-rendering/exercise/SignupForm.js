94732d71bf880ae834bd928471e33ac063081a91
Bao Nguyen <bao.cnguyen2@gmail.com>
2020-01-12 18:13:04 -0800

    finish lesson 1

--- lessons/01-rendering/exercise/SignupForm.js
+++ lessons/01-rendering/exercise/SignupForm.js
@@ -1,12 +1,39 @@
-import React, { Fragment, useState } from "react"
+import React, { Fragment } from "react"
 import VisuallyHidden from "@reach/visually-hidden"
-import { signup } from "app/utils"
 import TabsButton from "app/TabsButton"
 import { FaDumbbell } from "react-icons/fa"
 import { DateFields, MonthField, DayField, YearField } from "app/DateFields"
-// import SignupForm from "./SignupForm.final"
-// export default SignupForm
+
+const TextInput = ({ id, label, type = "text" }) => {
+  return (
+    <Fragment>
+      <VisuallyHidden>
+        <label htmlFor={id}>{label}</label>
+      </VisuallyHidden>
+      <input className="inputField" id={id} type={type} placeholder={label} />
+    </Fragment>
+  )
+}
 
 export default function SignupForm() {
-  return <form className="SignupForm">{/* Your work here */}</form>
+  return (
+    <form className="SignupForm">
+      <TextInput label="Display name" id="displayName" />
+      <TextInput label="Photo URL" id="photoURL" />
+      <TextInput label="Email" id="email" />
+      <TextInput label="password" id="password" type="password" />
+      <p>
+        <span aria-hidden="true">Start:</span>{" "}
+        <DateFields value={new Date()}>
+          <MonthField aria-label="Start Month" /> /{" "}
+          <DayField aria-label="Start Day" /> /{" "}
+          <YearField start={2018} end={2019} aria-label="Start year" />
+        </DateFields>
+      </p>
+      <TabsButton>
+        <FaDumbbell />
+        <span>Sign Up</span>
+      </TabsButton>
+    </form>
+  )
 }
