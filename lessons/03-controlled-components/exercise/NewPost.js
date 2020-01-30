549393475148980017a1b62cd99e7508e01b33f6
Bao Nguyen <bao.cnguyen2@gmail.com>
2020-01-30 15:49:45 -0800

    finish lesson 03

--- lessons/03-controlled-components/exercise/NewPost.js
+++ lessons/03-controlled-components/exercise/NewPost.js
@@ -5,25 +5,32 @@ import Minutes from "app/Minutes"
 import { FaDumbbell } from "react-icons/fa"
 import RecentPostsDropdown from "app/RecentPostsDropdown"
 
+// import NewPostFinal from "./NewPost.final"
+// export default NewPostFinal
+
 const errorClass = "NewPost_error"
 
 const MAX_MESSAGE_LENGTH = 200
 
 export default function NewPost({ takeFocus, date, onSuccess, showAvatar }) {
   const [{ auth }] = useAppState()
+  const [message, setMessage] = useState("Ran around the lake.")
+  const messageTooLong = message.length > MAX_MESSAGE_LENGTH
 
   return (
-    <div className="NewPost">
+    <div className={"NewPost" + (messageTooLong ? ` ${errorClass}` : "")}>
       {showAvatar && <Avatar uid={auth.uid} size={70} />}
       <form className="NewPost_form">
         <textarea
           className="NewPost_input"
           placeholder="Tell us about your workout!"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
         />
         <div className="NewPost_char_count">
-          0/{MAX_MESSAGE_LENGTH}
+          {message.length}/{MAX_MESSAGE_LENGTH}
         </div>
-        <RecentPostsDropdown uid={auth.uid} onSelect={(message) => {}}/>
+        <RecentPostsDropdown uid={auth.uid} onSelect={message => {}} />
         <div className="NewPost_buttons">
           <Minutes date={date} />
           <div>
